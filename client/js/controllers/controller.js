83316c31c58a9c560422d726bf5da704bfb6d918
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-01-16 15:47:36 -0500

    Hard Code User name in Angular

--- client/js/controllers/controller.js
+++ client/js/controllers/controller.js
@@ -1,3 +1,12 @@
 function userController($scope){
 	$scope.userCount = 10;
+
+	$scope.users = [
+	{name: "User 1"},
+	{name: "User 2"},
+	{name: "User 3"},
+	{name: "User 4"},
+	{name: "User 5"},
+	{name: "User 6"}
+	]
 }
