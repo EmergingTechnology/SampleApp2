fc7e9a188abb9693d939af02bdee8801db6f07c9
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-01-16 16:00:54 -0500

    Adding User Lists Dynamically

--- client/js/controllers/controller.js
+++ client/js/controllers/controller.js
@@ -1,5 +1,5 @@
 function userController($scope){
-	$scope.userCount = 10;
+	//$scope.userCount = 10;
 
 	$scope.users = [
 	{name: "User 1"},
@@ -9,4 +9,9 @@ function userController($scope){
 	{name: "User 5"},
 	{name: "User 6"}
 	]
+
+	$scope.createUser = function(){
+		$scope.users.push({name: $scope.userName});
+		$scope.userName = '';
+	}
 }
