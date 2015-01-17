f9ab64732d199e029827a134254d575d115fd1f2
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-01-17 00:31:56 -0500

    minor update

--- client/js/controllers/controller.js
+++ client/js/controllers/controller.js
@@ -9,7 +9,10 @@ function userController($scope){
 	}
 }
 
+
+
 function change(){
 	document.getElementById("demo").innerHTML = "You clicked a user name!!!";
 	setInterval(function(){document.getElementById("demo").innerHTML = ""},1700);
+
 }
