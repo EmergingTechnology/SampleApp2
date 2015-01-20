41f2f3a1d39cbc010ffbe591ad0ab962358da772
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-01-20 00:04:42 -0500

    Updated Controller for only Javascript functionality

--- client/js/controllers/controller.js
+++ client/js/controllers/controller.js
@@ -1,15 +1,3 @@
-function userController($scope){
-
-	$scope.users = []
-
-	$scope.createUser = function(){
-		$scope.users.push({name: $scope.userName});
-		$scope.userName = '';
-	}
-}
-
-
-
 function change(){
 	document.getElementById("demo").innerHTML = "You clicked a user name!!!";
 	setInterval(function(){document.getElementById("demo").innerHTML = ""},1700);
