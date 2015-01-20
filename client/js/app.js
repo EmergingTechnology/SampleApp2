c9968af8e71aa04ff772ace38b14aa4a04a898a4
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-01-20 00:03:20 -0500

    Updated app.js

--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,11 @@
-var app = angular.module('userApp', ['ngResource'])
+var app = angular.module('userApp', ['ngResource']);
+
+app.controller('userController', ['$scope', '$resource',
+function ($scope, $resource){
+  $scope.users = []
+
+  $scope.createUser = function(){
+    $scope.users.push({name: $scope.userName});
+    $scope.userName = '';
+  }
+}]);
